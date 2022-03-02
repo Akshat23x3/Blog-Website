4e33988c08e5d8ad8527148b32b376f07dc9f116
Akshat69 <59080228+Akshat69@users.noreply.github.com>
2022-03-02 13:53:03 +0530

    Added MongoDB

--- api/index.js
+++ api/index.js
@@ -1,1 +1,20 @@
-const
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(console.log("Connected to MongoDB")).catch(err => console.log(err));
+
+console.log("Hello....");
+
+app.use("/lama", (req, res) => {
+    console.log("This is Blog Website....");
+})
+
+app.listen("5008", () => {
+    console.log("Backend is running.")
+});
